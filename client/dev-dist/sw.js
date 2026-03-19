1ce108a55a8106ae6be5adafbc246faa3df07181
himanshujha-e-sutra <himanshujha@e-sutra.com>
2026-03-19 14:02:45 +0530

    dynamicdashboard

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e61anm1rq7"
+    "revision": "0.8p4plbi4ebg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
